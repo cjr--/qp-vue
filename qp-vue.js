052ba3eec3e086bacfac8f181a7ea7ef45b33ea2
Chris Richardson <cjr@hotmail.co.uk>
2017-05-20 00:25:25 +0100

    vue integration

--- qp-vue.js
+++ qp-vue.js
@@ -1,14 +1,34 @@
 define(module, function(exports, require, make) {
 
   var qp = require('qp-utility');
-  var vue = require('vue');
+  var Vue = require('vue');
+  var Vuex = require('vuex');
+  var VueRouter = require('vue-router');
+
+  Vue.config.productionTip = false;
 
   exports({
 
     ns: 'qp-vue',
 
+    create: function(o) {
+      qp.ready(function() { new Vue(o); });
+    },
+
+    store: function(o) {
+      Vue.use(Vuex);
+      return new Vuex.Store(o);
+    },
+
+    router: function(o) {
+      Vue.use(VueRouter);
+      return new VueRouter(qp.options(o, {
+        mode: 'history'
+      }));
+    },
+
     register: function(view, vm) {
-      vue.component(vm.name, qp.assign(vm, view));
+      Vue.component(vm.name, qp.assign(vm, view));
     }
 
   });
