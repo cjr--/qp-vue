f6afda889a194e9c791f8ddb2393caedd6bbeb7b
Chris Richardson <cjr@hotmail.co.uk>
2018-01-19 15:32:21 +0000

    vuex modules & store / router sync

--- qp-vue.js
+++ qp-vue.js
@@ -15,6 +15,8 @@ define(module, function(exports, require) {
         var component_name = o.render;
         o.render = function(h) { return h(component_name); };
       }
+      if (o.store) o.created = function() { this.$store.dispatch('initialise'); };
+      this.create_store_route(o.store, o.router);
       var vue = new Vue(o);
       qp.ready(function() { vue.$mount('#main'); });
     },
@@ -23,15 +25,16 @@ define(module, function(exports, require) {
       Vue.use(plugin);
     },
 
-    router: function(o) {
-      Vue.use(VueRouter);
-      return new VueRouter(qp.options(o, {
-        mode: 'history'
-      }));
-    },
-
     make: function(o) {
       var ns = o.ns;
+      if (o.computed ) {
+        if (o.computed.map_getters) qp.assign(o.computed, Vuex.mapGetters(qp.delete_key(o.computed, 'map_getters', {})));
+        if (o.computed.map_state) qp.assign(o.computed, Vuex.mapState(qp.delete_key(o.computed, 'map_state', {})));
+      }
+      if (o.methods) {
+        if (o.methods.map_actions) qp.assign(o.methods, Vuex.mapActions(qp.delete_key(o.methods, 'map_actions', {})));
+        if (o.methods.map_mutations) qp.assign(o.methods, Vuex.mapMutations(qp.delete_key(o.methods, 'map_mutations', {})));
+      }
       return exports(ns, this.extend(o));
     },
 
@@ -45,23 +48,70 @@ define(module, function(exports, require) {
 
     store: function(o) {
       Vue.use(Vuex);
-      if (o.mixins) {
-        qp.each(o.mixins, function(mixin) {
-          if (mixin) {
-            o.state = qp.assign(o.state, mixin.state);
-            o.getters = qp.assign(o.getters, mixin.getters);
-            o.mutations = qp.assign(o.mutations, mixin.mutations);
-            o.actions = qp.assign(o.actions, mixin.actions);
-          }
-        }.bind(this));
-      }
       return exports(o.ns, new Vuex.Store(o));
     },
 
-    map_state: function() { return Vuex.mapState.apply(null, arguments); },
-    map_getters: function() { return Vuex.mapGetters.apply(null, arguments); },
-    map_actions: function() { return Vuex.mapActions.apply(null, arguments); },
-    map_mutations: function() { return Vuex.mapMutations.apply(null, arguments); }
+    /* VueRouter */
+
+    router: function(o) {
+      Vue.use(VueRouter);
+      return new VueRouter(qp.options(o, {
+        mode: 'history'
+      }));
+    },
+
+    /* Vuex - VueRouter - Sync */
+
+    // https://github.com/vuejs/vuex-router-sync
+    create_store_route: function(store, router) {
+      if (store && router) {
+        var is_time_traveling = false;
+        var currentPath = null;
+
+        store.registerModule('route', {
+          namespaced: true,
+          state: clone_route(router.currentRoute),
+          mutations: {
+            route_changed: (state, transition) => store.state.route = clone_route(transition.to, transition.from)
+          }
+        });
+
+        var unwatch = store.watch(
+          (state) => state['route'],
+          (route) => {
+            var fullPath = route.fullPath;
+            if (fullPath === currentPath) return;
+            if (currentPath != null) {
+              is_time_traveling = true;
+              router.push(route);
+            }
+            currentPath = fullPath;
+          },
+          { sync: true }
+        );
+
+        var unhook = router.afterEach((to, from) => {
+          if (is_time_traveling) {
+            is_time_traveling = false;
+            return;
+          }
+          currentPath = to.fullPath;
+          store.commit('route/route_changed', { to: to, from: from });
+        });
+
+        function clone_route(to, from) {
+          var clone = { name: to.name, path: to.path, hash: to.hash, query: to.query, params: to.params, fullPath: to.fullPath, meta: to.meta }
+          if (from) clone.from = clone_route(from)
+          return Object.freeze(clone)
+        }
+
+        function destroy_store_route() {
+          if (unhook) unhook();
+          if (unwatch) unwatch();
+          store.unregisterModule('route');
+        }
+      }
+    }
 
   });
 
