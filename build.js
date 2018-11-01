a0d5ab1dbdc7cfef23dbcbf334d608e34b6646ed
cjr-- <cjr@hotmail.co.uk>
2018-11-01 14:35:55 +0000

    build improvements

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
+require('qp-define');
+
 var path = require('path');
 var fs = require('fs');
 var uglify = require('uglify-es');
+var term = require('qp-library/term');
+
+term.set_title('qp-vue - build');
 
 var result = uglify.minify(
   fs.readFileSync(path.join(__dirname, 'qp-vue.js'), 'utf8'),
